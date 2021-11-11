2efd91f925b0f81d3f567f05423752b0d190eb0a
eisa <g45@outlook.com>
2021-11-11 19:21:09 +0200

    Player Movement

---
+++ spacey war/Assets/PlayerControls.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    [SerializeField] float controlSpeed = 1f;
+    void Update()
+    {
+        float xThrow = Input.GetAxis("Horizontal") * Time.deltaTime * controlSpeed;
+
+        float yThrow = Input.GetAxis("Vertical") * Time.deltaTime * controlSpeed;
+
+        transform.localPosition = new Vector3 (
+            transform.localPosition.x + xThrow,
+            transform.localPosition.y + yThrow,
+            transform.localPosition.z
+        );
+    }
+}
