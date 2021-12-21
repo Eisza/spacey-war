26eaca2ae15cd5ddf8a7abbea15f343e3f10fc15
eisa <g45@outlook.com>
2021-12-21 23:49:05 +0200

    UI and score system

--- spacey war/Assets/Enemy.cs
+++ spacey war/Assets/Enemy.cs
@@ -6,11 +6,30 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject EnemyExplosion;
     [SerializeField] Transform IARuntime;
-    // Start is called before the first frame update
+    ScoreBoard scoreBoard;
+    
+    void Start()
+    {
+        scoreBoard = FindObjectOfType<ScoreBoard>();
+    }
+    
     void OnParticleCollision(GameObject other)
+    {
+        ProcessScore();
+        KillEnemy();
+    }
+
+
+    private void ProcessScore()
+    {
+        scoreBoard.AddScore(25);
+    }
+
+    private void KillEnemy()
     {
         GameObject vfx = Instantiate(EnemyExplosion, this.transform.position, Quaternion.identity);
         vfx.transform.parent = IARuntime;
+
         Destroy(gameObject);
     }
 }
